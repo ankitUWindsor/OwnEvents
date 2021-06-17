d744880cae3cca8a93579cbe685dc13514062de5
ankit9077 <ankit.bajaj000@gmail.com>
2021-06-17 09:19:30 -0400

    home component added and routes for home module made

--- webApp/src/app/app-routing.module.ts
+++ webApp/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
