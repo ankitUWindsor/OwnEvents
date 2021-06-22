6b6a924aa5b77120ec8a02861ceabe694bdc4253
RushikSantoki <santokir@uwindsor.ca>
2021-06-22 21:23:43 +0530

    Forget Password with Email verification

--- Api/models/user.js
+++ Api/models/user.js
@@ -13,14 +13,31 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-    },
+    },  
     userType:{
         type: Number,
         enum: [1, 2],
         default: 1,
+    },
+    interestsCategories: {
+        type: Number,
+        enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
     }
 });
-
+const interestsCategories = {Online_Streaming: 1, 
+    Ourdoor_events_Workshops: 2,
+    Comedy_Shows: 3,
+     Kids: 4,
+     Performance: 5,
+     Screening: 6,
+     Music_Shows: 7,
+     Concerts: 8,
+     Vaccination: 9,
+     Talks: 10,
+     Celebrity_Wishes: 11,
+     Award_Shows: 12,
+     Meetups: 13,
+     Photography: 14}
 const UserType = {
     Organizer: 1,
     Audience: 2
--- Api/routes/auth.js
+++ Api/routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const atob = require('atob');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
+const mailService = require('../service/mailer');
 
 router.post('/register', async (req, res) => {
     try {
@@ -41,6 +42,7 @@ router.post('/register', async (req, res) => {
             authToken: token
         });
     } catch (err) {
+        console.log('err', err);
         res.status(400).send(err);
     }
 });
@@ -80,5 +82,95 @@ router.post('/login', async (req, res) => {
         res.status(400).send(err);
     }
 });
+
+
+router.post('/forget-password', async (req, res) => {
+    try {
+
+        // check request have expected data or not
+        if (!req.body.email || !req.body.userType)
+            return res.status(400).send({
+                success: 0,
+                message: 'Missing some input data.'
+            });
+
+        const userType = req.body.userType
+        const user = await User.findOne({
+            email: req.body.email,
+            userType
+        });
+        if (!user && user.email) return res.status(404).send({
+            success: 0,
+            message: 'User not found'
+        });
+
+        const forgetPasswordToken = jwt.sign({
+            _id: user._id,
+            isForgetPassword: true,
+            createdAt: Date.now()
+        }, process.env.TOKEN_SECRET, {
+            expiresIn: '4h'
+        })
+
+        const link = `${process.env.FRONT_URL}reset-password/${forgetPasswordToken}`
+
+        try {
+            await mailService.sendMail({to: user.email, subject: 'Forget Password', text: link})
+
+            res.send({
+                success: 1,
+                message: 'Mail is sent on your email account for the reset password.',
+            });
+
+        } catch (e) {
+            res.statusCode(500).send({
+                success: 1,
+                message: 'Error in mail send',
+            });
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.post('/reset-password', async (req, res) => {
+    try {
+        // check request have expected data or not
+        if (!req.headers['reset-token'] || !req.body.password)
+            return res.status(400).send({
+                success: 0,
+                message: 'Missing some input data.'
+            });
+
+        try {
+            const decodedData = jwt.verify(req.headers['reset-token'], process.env.TOKEN_SECRET);
+            const user = await User.findOne({
+                _id: decodedData._id,
+            });
+            if (!user) return res.status(404).send({
+                success: 0,
+                message: 'User not found'
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(atob(req.body.password), salt);
+
+            user.password = hashedPassword;
+
+            await user.save();
+
+            res.send({
+                success: 1,
+                message: 'Password set successfully',
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(400).send('Invalid Token');
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
 
 module.exports = router;
