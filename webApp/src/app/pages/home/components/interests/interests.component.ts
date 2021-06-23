cb225252588cffbd4711557bc6b989ce8f06478d
ankit9077 <ankit.bajaj000@gmail.com>
2021-06-23 16:00:12 -0400

    matdialog style changed and interests component matdialog refs made and openned in home component

--- webApp/src/app/pages/home/components/interests/interests.component.ts
+++ webApp/src/app/pages/home/components/interests/interests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-interests',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<InterestsComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  CloseDialog(): void {
+    this.dialogRef.close();
   }
 
 }
