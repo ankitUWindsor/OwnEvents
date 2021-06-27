089ee5f328e57b2683fdc781d5964efb1eff52b1
ankit9077 <ankit.bajaj000@gmail.com>
2021-06-27 18:43:45 -0400

    profile component changes made

--- webApp/src/app/pages/home/components/profile/profile.component.ts
+++ webApp/src/app/pages/home/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
+import { InterestsCategory } from 'src/assets/enums';
+import { UserService } from './../../../../services/user/user.service';
 import { Component, OnInit } from '@angular/core';
+import { EventTypes } from 'src/assets/constants';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  GetInterestsList(): string {
+    let str = '';
+    this.userService.user.interests.forEach((value, index) => {
+      const foundAt = EventTypes.findIndex(item => item.enumValue === InterestsCategory[value]);
+      str += EventTypes[foundAt].text;
+      if (index !== this.userService.user.interests.length - 1) {
+        str += ', ';
+      }
+    });
+    if (str.length > 20) {
+      str = str.slice(0, 17) + '...';
+    }
+    return str;
   }
 
 }
